d65b94f4a1363f2726bbb00440cb21d2813ae7e0
aeorherhe <aeorherhe@gmail.com>
2023-05-14 16:46:52 +0100

    Subscribe msg

--- js/script.js
+++ js/script.js
@@ -2,17 +2,22 @@
 
 const submitBtn = document.querySelector(".submit-icon");
 const article = document.querySelector("article");
-const inputField = document.getElementById("inputfield");
+const inputField = document.getElementById("input-field");
+const label = document.querySelector(".label");
+const mailRegex = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
 
-console.log(submitBtn);
-console.log(article.classList);
-
-submitBtn.addEventListener("click", function () {
-  if (
-    !inputField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)
-  ) {
+function validateEmail() {
+  if (!inputField.value.match(mailRegex)) {
     article.classList.add("show-error-msg");
   } else {
     article.classList.remove("show-error-msg");
+  }
+}
+
+inputField.addEventListener("input", validateEmail);
+
+submitBtn.addEventListener("click", function () {
+  if (inputField.value.match(mailRegex)) {
+    label.classList.add("show-msg");
   }
 });
