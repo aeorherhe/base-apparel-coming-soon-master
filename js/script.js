8046fdaa981b0e8ab556bea9c6a2ea2c85d3bfdb
aeorherhe <aeorherhe@gmail.com>
2023-05-14 15:48:41 +0100

    JS email validation

--- js/script.js
+++ js/script.js
@@ -2,10 +2,17 @@
 
 const submitBtn = document.querySelector(".submit-icon");
 const article = document.querySelector("article");
+const inputField = document.getElementById("inputfield");
 
 console.log(submitBtn);
 console.log(article.classList);
 
 submitBtn.addEventListener("click", function () {
-  article.classList.toggle("show-error-msg");
+  if (
+    !inputField.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)
+  ) {
+    article.classList.add("show-error-msg");
+  } else {
+    article.classList.remove("show-error-msg");
+  }
 });
